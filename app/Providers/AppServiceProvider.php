eedac659b8bb393f1a0acb059d1df5de0f84554b
OsGAr-CJ <o_garibay74@yahoo.com.mx>
2026-04-14 12:30:04 -0600

    Restaurar AppServiceProvider.php desde cero

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,28 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Schema;//ajustes necesario para que no haya problema
-                                       // con la migracion de B.D
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        
         if ($this->app->environment('production')) {
-        URL::forceScheme('https');
-    }
+            URL::forceScheme('https');
+        }
     }
 }
-
